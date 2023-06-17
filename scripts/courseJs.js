f1cec6491f2dc61ad3ec1e4b5c79051e82bd328f
Bardya Vahydy <vahydy.bardya97@gmail.com>
2023-06-17 20:44:05 +0330

    set title for each page

--- scripts/courseJs.js
+++ scripts/courseJs.js
@@ -1,10 +1,24 @@
 const $ = document
+const title = $.getElementById('title')
 const header = $.querySelector('.header')
 const menuHamburger = header.querySelector('.menu-hamburger')
 const menuCrossIcon = header.querySelector('.btn-menu-cross__icon')
 const totalContainerMenu = header.querySelector('.total-container-menu') 
 
 const footerLogo = $.querySelector('.footer-logo')
+
+let productsInfo = {
+    TypeScript:{ 
+        titlePage:'دوره مقدماتی تا پیشرفته تایپ اسکریپت | بوتواستارت' , 
+    },
+    JavaScript:{ 
+        titlePage:'آموزش مقدماتی تا پیشرفته جاوااسکریپت | دوره پروژه محور Javascript | بوتواستارت' ,
+    }
+}
+let productsInfoKeys = Object.keys(productsInfo)
+
+let locatiocSreachPharams = new URLSearchParams(location.search)
+let coursePharam = locatiocSreachPharams.get('course')
 
 ////add classes////
 
@@ -17,6 +31,19 @@ function removeActiveClass(element , classname){
 }
 
 ////functions////
+
+function setInfoPage(){
+    productsInfoKeys.forEach( productsInfoKey =>{
+        if(coursePharam === productsInfoKey){
+            setTitlePage(productsInfo[productsInfoKey].titlePage)
+        }
+    } )
+}
+setInfoPage()
+
+function setTitlePage(titlePage){
+    title.innerText = titlePage 
+}
 
 function headerHandler(){
     addActiveClass(header , 'header-active')
